74ed71caa16ac786587a0f19d7021a6d34993e6c
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-10-07 16:16:01 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -201,7 +201,7 @@ io.on('connection', (socket) => {
 
     setInterval(() => {
         io.emit('gameState', { players: gameWorld.getPlayers() });
-    }, 1000 / 30); // 30 times per second
+    }, 1000 / 1); // updates times per second
 });
 
 const PORT = process.env.PORT || 3000;
