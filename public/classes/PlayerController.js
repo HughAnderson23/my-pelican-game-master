6d1ae92e80013cebf02a1ab1b150756f3d4883ea
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-10-07 15:39:27 -0400

    Reduced client side updates

--- public/classes/PlayerController.js
+++ public/classes/PlayerController.js
@@ -41,7 +41,7 @@ export class PlayerController {
                 this.lerpFactor
             );
 
-            char.update(16); // Assume 60 FPS, so deltaTime is about 16ms
+            char.update(1); // Assume 60 FPS, so deltaTime is about 16ms
         });
 
         this.checkForMerge();
